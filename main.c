fb77dd9dee11e64527b02b5cec4133d79fd049ce
devops-intellipaat <hemant@intellipaat.com>
2019-01-18 02:28:17 +0000

    added feature A

--- main.c
+++ main.c
@@ -1,15 +1,18 @@
-pseudo code
-{
-Initial Function()
-{
-  //Sample Code
+#include <stdio.h>
+#include <stdlib.h>
+
+// SECURITY PATCH: Input sanitization
+void sanitize_input(char *input, int max_len) {
+    if (input == NULL) exit(EXIT_FAILURE);
 }
 
-Initial Security()
-{
- //Sample Code
+// Feature 1
+void feature1() {
+    printf("Feature 1 is running!\n");
 }
 
-
-
+int main() {
+    printf("Welcome to Zendriix Application\n");
+    feature1();
+    return 0;
 }
